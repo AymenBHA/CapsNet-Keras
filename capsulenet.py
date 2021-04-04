3240ee7179d423e56214287a6597e30ae5ec9fff
Aymen Boulila <33230135+AymenBHA@users.noreply.github.com>
2021-04-05 00:12:06 +0100

    Update capsulenet.py
    removed shitty new args naming

--- capsulenet.py
+++ capsulenet.py
@@ -208,28 +208,9 @@ if __name__ == "__main__":
     from tensorflow.keras import callbacks
 
     # setting the hyper parameters
-    parser = argparse.ArgumentParser(description="Capsule Network on MNIST.")
-    parser.add_argument('--epochs', default=50, type=int)
-    parser.add_argument('--batch_size', default=100, type=int)
-    parser.add_argument('--lr', default=0.001, type=float,
-                        help="Initial learning rate")
-    parser.add_argument('--lr_decay', default=0.9, type=float,
-                        help="The value multiplied by lr at each epoch. Set a larger value for larger epochs")
-    parser.add_argument('--lam_recon', default=0.392, type=float,
-                        help="The coefficient for the loss of decoder")
-    parser.add_argument('-r', '--routings', default=3, type=int,
-                        help="Number of iterations used in routing algorithm. should > 0")
-    parser.add_argument('--shift_fraction', default=0.1, type=float,
-                        help="Fraction of pixels to shift at most in each direction.")
-    parser.add_argument('--debug', action='store_true',
-                        help="Save weights by TensorBoard")
-    parser.add_argument('--save_dir', default='./result')
-    parser.add_argument('-t', '--testing', action='store_true',
-                        help="Test the trained model on testing dataset")
-    parser.add_argument('--digit', default=5, type=int,
-                        help="Digit to manipulate")
-    parser.add_argument('-w', '--weights', default=None,
-                        help="The path of the saved weights. Should be specified when testing")
+    from types import SimpleNamespace
+    args = SimpleNamespace(batch_size=100, debug=0, epochs=1, is_training=1, lam_recon=0.392, lr=0.001, 
+            num_routing=3, save_dir='./result', shift_fraction=0.1, weights=None, lr_decay=0.9)
     args = parser.parse_args()
     print(args)
 
@@ -249,7 +230,7 @@ if __name__ == "__main__":
     # train or test
     if args.weights is not None:  # init the model weights with provided one
         model.load_weights(args.weights)
-    if not args.testing:
+    if args.is_training:
         train(model=model, data=((x_train, y_train), (x_test, y_test)), args=args)
     else:  # as long as weights are given, will run testing
         if args.weights is None:
